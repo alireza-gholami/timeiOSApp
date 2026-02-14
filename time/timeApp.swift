fd20c22be42b342c84840f74d2245561c9c75c0e
alireza <financerz52@yahoo.com>
2026-02-14 18:07:48 +0100

    Initial Commit

---
+++ time/timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeApp.swift
+//  time
+//
+//  Created by Alireza on 14.02.26.
+//
+
+import SwiftUI
+
+@main
+struct timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
